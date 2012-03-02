cd5b1bc269ff89d0e49ea662c878a2057be37a8c
Pascal MUANG-KHOT <mail@mkpascal.net>
2012-03-02 18:05:17 +0100

    First version using SPECKLED_MELON as the default item

--- src/com/github/mkpascal/CrazyFoodCraft/CrazyFoodCraft.java
+++ src/com/github/mkpascal/CrazyFoodCraft/CrazyFoodCraft.java
@@ -1,17 +1,31 @@
 package com.github.mkpascal.CrazyFoodCraft;
 
 import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
- 
+
 public class CrazyFoodCraft extends JavaPlugin {
- 
+	
 	Logger log = Logger.getLogger("Minecraft");
  
+	public final CrazyFoodCraftBlockListener blockListener = new CrazyFoodCraftBlockListener();
+	
 	public void onEnable(){
 		log.info("CrazyFoodCraft enabled!");
+		
+		PluginManager pm = getServer().getPluginManager();
+	
+		pm.registerEvents(this.blockListener, this);
 	}
  
 	public void onDisable(){
 		log.info("CrazyFoodCraft disabled.");
 	}
+
 }
+
+
+
+
+
---
+++ src/com/github/mkpascal/CrazyFoodCraft/CrazyFoodCraftBlockListener.java
@@ -0,0 +1,49 @@
+package com.github.mkpascal.CrazyFoodCraft;
+
+import java.util.Random;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;;
+
+public class CrazyFoodCraftBlockListener implements Listener {
+
+	
+		@EventHandler(priority = EventPriority.NORMAL) 
+        public void interactFood(PlayerInteractEvent event){
+  
+            Player player = event.getPlayer();
+
+			if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
+			{
+				
+				if(player.getItemInHand().getType().equals(Material.SPECKLED_MELON))
+				{
+					
+					Random rand = new Random();
+					int x = rand.nextInt(10);
+					 
+					   		if(x < 5)
+					   		{
+					   			player.sendMessage("Ouch. This wasn't a good one.");
+					   			player.setFireTicks(10000);
+					   		}
+					   		else
+					   		{
+				                player.setHealth(20);
+				                player.setFoodLevel(20);
+					   			player.sendMessage("Nice, it heals you!");	
+					   			
+					   		}
+					   
+				}
+				
+			}	
+			
+        }
+		
+}
